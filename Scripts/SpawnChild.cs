d603c808a2e5849c50d81cac3748b56c14611197
Otto Leppänen <jkvo17@gmail.com>
2025-03-03 02:49:39 +0200

    Banana and randomized trash spawn

--- Scripts/SpawnChild.cs
+++ Scripts/SpawnChild.cs
@@ -3,19 +3,40 @@ using System;
 
 public partial class SpawnChild : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-
-	[Export] PackedScene scene = null;
-
+	[Export] PackedScene trash1 = null;
+	[Export] PackedScene trash2 = null;
+	[Export] float minSpawnTime = 10.0f;
+	[Export] float maxSpawnTime = 30.0f;
+	private Random random = new Random();
+	
 	public override void _Ready()
 	{
-		GetNode<Timer>("Timer").Timeout += spawn;
+		SetRandomSpawnTime();
 	}
-	private void spawn()
+
+	private void spawnObjects()
 	{
-		PathFollow2D s = scene.Instantiate<PathFollow2D>();
-		GetTree().Root.GetNode<Path2D>("Main/Path2D").AddChild(s);
+		Path2D path = GetTree().Root.GetNode<Path2D>("Main/Path2D");
+
+		PackedScene scene = (random.Next(2) == 0) ? trash1 : trash2;
+		PathFollow2D instance = scene.Instantiate<PathFollow2D>();
+
+		path.AddChild(instance);
+	}
+
+	private void SetRandomSpawnTime()
+	{
+		float randomTime = (float)(random.NextDouble() * (maxSpawnTime - minSpawnTime) + minSpawnTime);
+		Timer timer = new Timer();
+		AddChild(timer);
+		timer.WaitTime = randomTime;
+		timer.OneShot = true;
+		timer.Timeout += () =>
+		{
+			spawnObjects();
+			SetRandomSpawnTime();
+			timer.QueueFree();
+		};
+		timer.Start();
 	}
 }
