c8415143d277477d7eb4ace19da167c142db2bb6
joe-vis <joel.visakorpi@gmail.com>
2025-03-17 11:17:40 +0200

    menu art + basket limits

--- Scripts/Basket.cs
+++ Scripts/Basket.cs
@@ -11,21 +11,29 @@ public partial class Basket : Node2D
     private RiverItem _inputType = RiverItem.None;
 
     private int _points = 0;
-
+    private int _neededPoints = 10;
     public override void _Ready()
     {
         _displayIcon.Texture = _displayTexture;
+        UpdatePoints();
+    }
+
+    private void UpdatePoints()
+    {
+        GetNode<RichTextLabel>("Score").Text = $"{_points} / {_neededPoints}";
     }
 
 
     public bool AddToBasket(Icon item)
     {
-        if (item.Type == _inputType)
+        if (item.Type != _inputType || _points >= _neededPoints)
         {
-            _points++;
-            return true;
+            return false;
         }
 
-        return false;
+        _points++;
+        UpdatePoints();
+        return true;
+
     }
 }
