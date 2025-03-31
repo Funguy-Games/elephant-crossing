0ca703a5ae51ac08e78b72985b24b4ba7d67d9e6
Joe-vis <joel.visakorpi@gmail.com>
2025-04-01 02:32:46 +0300

    level selection menu + scene button script

--- Levels/SceneManager.cs
+++ Levels/SceneManager.cs
@@ -4,23 +4,54 @@ using System;
 public partial class SceneManager : Node
 {
     private FadeCanvas _fade = null;
+    private string _nextScene = "";
+
     public override void _Ready()
     {
         _fade = GetParent().GetNode<FadeCanvas>("FadeCanvas");
     }
+
     public void LoadLevel1()
     {
+        _nextScene = "res://Levels/main.tscn";
+        _fade.FadedOut += ChangeScene;
+        _fade.FadeOut();
+    }
+
+    public void LoadLevelSelector()
+    {
+        _nextScene = "res://Levels/LevelSelection.tscn";
         _fade.FadedOut += ChangeScene;
         _fade.FadeOut();
     }
 
     public void LoadMainMenu()
     {
-        GetTree().ChangeSceneToFile("res://Levels/menu.tscn");
+        _nextScene = "res://Levels/menu.tscn";
+        _fade.FadedOut += ChangeScene;
+        _fade.FadeOut();
+    }
+    /// <summary>
+    /// Changes the current scene based on a given scene name.
+    /// Before the scene changes this method attempts to fade out the current scene.
+    /// </summary>
+    /// <param name="scene">Name of the next scene</param>
+    public void LoadScene(string scene)
+    {
+        _nextScene = scene;
+
+        if(_fade == null)
+        {
+            ChangeScene();
+            return;
+        }
+
+        _fade.FadedOut += ChangeScene;
+        _fade.FadeOut();
     }
 
     private void ChangeScene()
     {
-        GetTree().ChangeSceneToFile("res://Levels/main.tscn");
+        GetTree().ChangeSceneToFile(_nextScene);
     }
 }
---
+++ Scripts/SceneButton.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public partial class SceneButton : BaseButton
+{
+    [Export]
+    private string _sceneName = "";
+
+
+
+}
