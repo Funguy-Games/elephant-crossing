0602541054e3b8e1a1e0fb466b54458c38161e82
Otto Leppänen <jkvo17@gmail.com>
2025-03-31 14:41:07 +0300

    Crocodile spawning

--- Scripts/SpawnChild.cs
+++ Scripts/SpawnChild.cs
@@ -1,16 +1,18 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 public partial class SpawnChild : Node2D
 {
-	[Export] PackedScene trash1 = null;
-	[Export] PackedScene trash2 = null;
 	[Export] PackedScene roar = null;
 	[Export] float minSpawnTime = 1.0f;
 	[Export] float maxSpawnTime = 4.0f;
 	[Export] private int _crocodileDelay = 5;
 	[Export] private Trash[] trashArray;
+	private float _crocoDileChance = 0;
+	private float _startChance = 0.3f;
+	[Export] private float _chanceIncrease = 0.5F;
 
 	private Random random = new Random();
 
@@ -22,11 +24,20 @@ public partial class SpawnChild : Node2D
 			if (trash.TrashAmount > 0 && trash.trashType != null)
 				trashList.Add(trash);
 		}
+
+		_crocoDileChance = _startChance;
 		SetRandomSpawnTime();
 	}
 
 	private void spawnObjects()
 	{
+		GD.Print(_crocoDileChance);
+
+		if (SpawnCrocodile())
+		{
+			return;
+		}
+
 		Path2D path = GetTree().Root.GetNode<Path2D>("Main/Path2D");
 
 		PackedScene scene = null;
@@ -50,6 +61,23 @@ public partial class SpawnChild : Node2D
 			PathFollow2D instance = scene.Instantiate<PathFollow2D>();
 			path.AddChild(instance);
 		}
+	}
+
+	private bool SpawnCrocodile()
+	{
+		float crocodileRandom = GD.Randf();
+		GD.Print(crocodileRandom);
+		if (crocodileRandom > _crocoDileChance)
+		{
+			_crocoDileChance += _chanceIncrease;
+			return false;
+		}
+
+		Path2D path = GetTree().Root.GetNode<Path2D>("Main/Path2D");
+		PathFollow2D instance = roar.Instantiate<PathFollow2D>();
+		path.AddChild(instance);
+		_crocoDileChance = _startChance;
+		return true;
 	}
 
 	private void SetRandomSpawnTime()
