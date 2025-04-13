1032197f57816435279ad4c34b45035fa28c1239
Otto Leppänen <jkvo17@gmail.com>
2025-04-13 22:36:09 +0300

    Actual game pausing and global background music

--- Levels/PauseMenu.cs
+++ Levels/PauseMenu.cs
@@ -27,6 +27,7 @@ public partial class PauseMenu : Control
 	{
 		isPaused = !isPaused;
 		Visible = isPaused;
+		GetTree().Paused = true;
 		button1._isMoving = true;
 		button2._isMoving = true;
 		button3._isMoving = true;
@@ -47,6 +48,7 @@ public partial class PauseMenu : Control
 	{
 		isPaused = !isPaused;
 		Visible = isPaused;
+		GetTree().Paused = false;
 		button1.ProgressRatio = button1._startingRatio;
 		button2.ProgressRatio = button2._startingRatio;
 		button3.ProgressRatio = button3._startingRatio;
@@ -64,6 +66,7 @@ public partial class PauseMenu : Control
 		_timer.OneShot = true;
 		_timer.Timeout += () =>
 		{
+			GetTree().Paused = false;
 			GetTree().ChangeSceneToFile("res://Levels/menu.tscn");
 			_timer.QueueFree();
 		};
