498fdeafbe5b179de4c961e161a1c85efce40a5b
Otto Leppänen <jkvo17@gmail.com>
2025-04-17 02:14:44 +0300

    Very crappy tutorial

---
+++ Tutorial.cs
@@ -0,0 +1,65 @@
+using Godot;
+using System;
+
+public partial class Tutorial : CanvasLayer
+{
+	[Export] CanvasLayer Buttons = null;
+	[Export] CanvasLayer Tutorial1 = null;
+	[Export] CanvasLayer Tutorial2 = null;
+	[Export] CanvasLayer Tutorial3 = null;
+	[Export] CanvasLayer Tutorial4 = null;
+	[Export] CanvasLayer Tutorial5 = null;
+	[Export] CanvasLayer Tutorial6 = null;
+
+	private int currentIndex = 0;
+	private CanvasLayer[] tutorials;
+
+	public override void _Ready()
+	{
+		Visible = true;
+		Buttons.Visible = true;
+
+		Engine.TimeScale = 0;
+
+		AudioServer.SetBusMute(2, true);
+
+		tutorials = new CanvasLayer[] { Tutorial1, Tutorial2, Tutorial3, Tutorial4, Tutorial5, Tutorial6 };
+
+		ShowCurrentTutorial();
+	}
+
+	private void ShowCurrentTutorial()
+	{
+		for (int i = 0; i < tutorials.Length; i++)
+		{
+			tutorials[i].Visible = (i == currentIndex);
+		}
+	}
+
+	private void GoForward()
+	{
+		if (currentIndex < tutorials.Length - 1)
+		{
+			currentIndex++;
+			ShowCurrentTutorial();
+		}
+	}
+
+	private void GoBack()
+	{
+		if (currentIndex > 0)
+		{
+			currentIndex--;
+			ShowCurrentTutorial();
+		}
+	}
+
+	private void ExitTutorial()
+	{
+		Engine.TimeScale = 1;
+		Visible = false;
+		tutorials[currentIndex].Visible = false;
+		Buttons.Visible = false;
+		AudioServer.SetBusMute(2, false);
+	}
+}
